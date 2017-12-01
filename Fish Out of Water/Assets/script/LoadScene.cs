4e5b2d6ec16816908c6fc517104269599c693918
Kim Huynh <kh.kimhuynh@gmail.com>
2017-12-01 14:59:18 -0500

    added some title animations
    not done the title animations yet though

---
+++ Fish Out of Water/Assets/script/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public string SceneToLoad = "";
+    public float Delay = 0;
+
+    void Start()
+    {
+        StartCoroutine(delayThenLoadScene());
+    }
+
+    IEnumerator delayThenLoadScene()
+    {
+        yield return new WaitForSeconds(Delay);
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
