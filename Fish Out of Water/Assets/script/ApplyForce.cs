f9993fa37a2b640ed52f19a1c78565917f368d6b
Kim Huynh <kh.kimhuynh@gmail.com>
2018-01-09 14:59:25 -0500

    added stuff up to the sling shot
    fix the sling shot

---
+++ Fish Out of Water/Assets/script/ApplyForce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    public KeyCode LeftTurnKey;
+    public Rigidbody2D rigidBody;
+    public float forceRight;
+    public float forceUp;
+    public float torque;
+
+    void Update()
+    {
+        if (Input.GetKey(LeftTurnKey))
+        {
+            rigidBody.AddTorque(torque);
+        }        
+    }
+
+    void Start()
+    {
+        rigidBody.AddForce(transform.right * forceRight);
+        rigidBody.AddForce(transform.up * forceUp);
+    }
+}
